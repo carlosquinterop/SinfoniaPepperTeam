e7443f376989f228603193c5139636f0d7a97cec
David Giraldo Cano <david.gc02@outlook.com>
2019-02-04 15:06:11 -0500

    [robot_toolkit] Add robot_interaction

---
+++ sinfonia_pepper_robot_toolkit/scripts/robot_interaction.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# license removed for brevity
+
+"""
+//======================================================================//
+//  This software is free: you can redistribute it and/or modify        //
+//  it under the terms of the GNU General Public License Version 3,     //
+//  as published by the Free Software Foundation.                       //
+//  This software is distributed in the hope that it will be useful,    //
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of      //
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE..  See the      //
+//  GNU General Public License for more details.                        //
+//  You should have received a copy of the GNU General Public License   //
+//  Version 3 in the file COPYING that came with this distribution.     //
+//  If not, see <http://www.gnu.org/licenses/>                          //
+//======================================================================//
+//                                                                      //
+//      Copyright (c) 2019 SinfonIA Pepper RoboCup Team                 //
+//      Sinfonia - Colombia                                             //
+//      https://sinfoniateam.github.io/sinfonia/index.html              //
+//                                                                      //
+//======================================================================//
+"""
+
+
+class RobotInteraction:
+
+    def __init__(self, ip):
+        pass
