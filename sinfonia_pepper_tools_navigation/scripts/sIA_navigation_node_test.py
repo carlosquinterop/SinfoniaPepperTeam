0db2a3c0f4c31465922ce44e3a708e9d31f1bd63
JuanDavidG1997 <44007825+JuanDavidG1997@users.noreply.github.com>
2019-02-12 23:52:54 -0500

    [Navigation] Updated Scripts for the navigation node

--- sinfonia_pepper_tools_navigation/scripts/sIA_navigation_node_test.py
+++ sinfonia_pepper_tools_navigation/scripts/sIA_navigation_node_test.py
@@ -5,32 +5,91 @@ import time
 import rospy
 from std_msgs.msg import String
 from geometry_msgs.msg import Vector3
+from geometry_msgs.msg import Quaternion
 from nav_msgs.msg import OccupancyGrid
 from sinfonia_pepper_tools_navigation.msg import doReactNav
+from sinfonia_pepper_tools_navigation.msg import location
+from sinfonia_pepper_tools_navigation.srv import GetMap
+from sinfonia_pepper_tools_navigation.srv import GetPlaceLocation
  
 global TESTTOPIC
-TESTTOPIC = "sIA_navigation"
+# Variable de prueba - EDITE EL VALOR DE TESTTOPIC POR LA FUNCIONALIDAD QUE DESEA PROBAR!
+TESTTOPIC = "sIA_test_set_place_location"
 
+# --- Calbacks ---
 def navigationTestNodeCallback(msg):
 	pass
 
+# --- Funcion principal ---
 def navigationTestNode():
-	rospy.init_node('navigation_test_node', anonymous=True)
-	rate = rospy.Rate(10)
-	if TESTTOPIC== "sIA_navigation":
-		testNavigation(rate)
+    rospy.init_node('navigation_test_node', anonymous=True)
+    rate = rospy.Rate(10)
+    
+    
+    if TESTTOPIC == "sIA_test_react_navigation":
+		testDoReactNavigation(rate)
 
-def testNavigation(rate):
-	pub= rospy.Publisher('sIA_start_navigation',doReactNav,queue_size=2)
-	rospy.Subscriber('map',OccupancyGrid, navigationTestNodeCallback)
+    if TESTTOPIC == "sIA_test_move_to_point":
+		testMoveToPoint(rate)
+
+    if TESTTOPIC == "sIA_test_get_map":
+		testGetMap(rate)
+    
+    if TESTTOPIC == "sIA_test_set_place_location":
+		testSetPlacesLocation(rate)
+    
+    if TESTTOPIC == "sIA_test_get_place_location":
+ #       testSetPlacesLocation(rate)
+   		testGetPlaceLocation(rate)
+
+# --- Funciones de pruebas ---
+def testDoReactNavigation(rate):
+	pub= rospy.Publisher('sIA_do_react_navigation', doReactNav, queue_size = 5)
 	msg = doReactNav()
-	msg.doReactNav = [True,True]
-	while pub.get_num_connections()==0:
+	msg.doReactNav = False
+	msg.updateMap = True
+	while not rospy.is_shutdown():
+		print(msg)
+		pub.publish(msg)
+        rate.sleep()
+
+def testMoveToPoint(rate):
+	pub = rospy.Publisher('sIA_move_to_point',Quaternion, queue_size = 5)
+	msg = Quaternion()
+	msg.x = 2.0
+	msg.y = 0.0
+	msg.z = 0.0
+	msg.w = 1.0
+	while not rospy.is_shutdown():
+		print(msg)
+		pub.publish()
 		rate.sleep()
 
-	pub.publish(msg.doReactNav)
-	#pub.publish([True,True])
-	rate.sleep()
+def testGetMap(rate):
+    rospy.wait_for_service('sIA_get_map')
+    getMap = rospy.ServiceProxy("sIA_get_map", GetMap)
+    response = getMap("Get Map", True).response
+    print(response)
+    
+def testSetPlacesLocation(rate):
+    pub= rospy.Publisher('sIA_set_places_location', location, queue_size = 5)
+    msg = location()
+    msg.name = "Living Room"
+    msg.x = 2 
+    msg.y = 2
+    msg.z = 0
+    msg.w = 2
+    while not rospy.is_shutdown():
+        print(msg)
+        pub.publish(msg)
+        rate.sleep()
+    
+def testGetPlaceLocation(rate):
+    rospy.wait_for_service('sIA_get_place_location')
+    placeLocation = rospy.ServiceProxy("sIA_get_places_location", GetPlaceLocation)
+    response = placeLocation("Get Place Location", "Living room").response
+    print(response)
+
 
 if __name__ =='__main__':
 	try:
