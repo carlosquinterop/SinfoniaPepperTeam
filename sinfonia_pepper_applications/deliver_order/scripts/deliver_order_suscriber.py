2c557a17466f89d505e3a20e17efe2b0c58fe52a
Bryan Betancur <betan2@hotmail.com>
2019-02-15 17:43:17 -0500

    [Applications][Interaction][Desicion_Making] Added applications take order, make order and give order...

--- sinfonia_pepper_applications/deliver_order/scripts/deliver_order_suscriber.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import rospy
-from std_msgs.msg import String
-orders = None
-def callback(data):
-    global orders
-    orders = data.data
-def suscribe_topic():
-    rospy.Subscriber("person_detection", String, callback)
